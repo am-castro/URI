cd11d6201deb62c9f745104513bae4e3bd83ccd0
Veganon <antoniomarcos.mc36@gmail.com>
2021-05-31 00:07:04 -0300

    Lista 1 URI
    Umil Bolt

--- 2863/2863.c
+++ 2863/2863.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main(){
-
+	
+	int T=0, i=0;
+	while(scanf("%i",&T)!=EOF){
+		double N[T];
+		for(i=1; i<=T; i++){
+			scanf("%lf",&N[i]);
+		}
+		for(i=1; i<=T; i++){
+			if(N[1]>N[i]){
+				N[1]=N[i];
+				printf("%.2f\n",N[i]);
+			}
+		}
+	}
     return 0;
 }

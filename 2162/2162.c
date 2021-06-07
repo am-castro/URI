c9bfcf25c4c17909a664e9104559ee051b62db99
Veganon <antoniomarcos.mc36@gmail.com>
2021-06-07 00:20:33 -0300

    10% de erro no uri

--- 2162/2162.c
+++ 2162/2162.c
@@ -8,17 +8,36 @@
  
 int main() {
 	
-	int N=0, pico=0, i=0;
+	int N=0, pico=1, i=0, j=0;
 	scanf("%d",&N);
  	int H[N];
- 	
-	for(i=1; i<=N; i++){
+ 	for(i=1; i<=N; i++){
 		scanf("%d",&H[i]);
 	}
-	for(i=2;i<=N;i++){
-		if(H[i-1]<H[i]){
-			printf("1\n");
-		}	
+ 	if(N==1){
+ 		printf("%d\n",pico);	
+	}
+	else if(N==2){
+		pico=0;
+ 		printf("%d\n",pico);	
+	}
+	else if(N>=3){
+		for(i=1; i<=N; i++){
+			if(H[i]==H[i+1]){
+				pico=0;
+				break;
+			}
+			else if(H[i]>H[i+1] && H[i+1]>H[i+2]){
+				pico=0;
+				break;
+			}
+			else if(H[i]<H[i+1] && H[i+1]<H[i+2]){
+				pico=0;
+				break;
+			}
+		}
+		
+		printf("%d\n",pico);
 	}
 	
 	return 0;
